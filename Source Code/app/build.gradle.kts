957da7802b0d1b1eb873ba351130660cf3e6fabe
Rafal Ozog <rafalozog@gmail.com>
2023-02-12 22:35:26 +0000

    Dependencies updates.

--- Source Code/app/build.gradle.kts
+++ Source Code/app/build.gradle.kts
@@ -33,30 +33,30 @@ dependencies {
     kapt("android.arch.lifecycle:compiler:1.1.1")
 
     implementation("androidx.appcompat:appcompat:1.3.0-alpha02")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.6.1")
 
-    implementation("com.google.dagger:dagger:2.27")
-    kapt("com.google.dagger:dagger-compiler:2.27")
+    implementation("com.google.dagger:dagger:2.44")
+    kapt("com.google.dagger:dagger-compiler:2.42")
     compileOnly("org.glassfish:javax.annotation:3.1.1")
 
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.42")
 
-    implementation("androidx.room:room-runtime:2.2.5")
-    kapt("androidx.room:room-compiler:2.2.5")
+    implementation("androidx.room:room-runtime:2.5.0")
+    kapt("androidx.room:room-compiler:2.5.0")
 
     testImplementation("org.mockito:mockito-inline:2.13.0")
     testImplementation("android.arch.core:core-testing:1.1.1")
 
-    implementation("com.squareup.retrofit2:retrofit:2.4.0")
+    implementation("com.squareup.retrofit2:retrofit:2.8.1")
     implementation("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
     implementation("com.squareup.retrofit2:converter-gson:2.3.0")
 
-    implementation("com.google.code.gson:gson:2.8.2")
+    implementation("com.google.code.gson:gson:2.9.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.1.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
